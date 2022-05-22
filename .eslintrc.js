17029542704adcbdf9381f59589f33c9bfc0355e
youngdanon <dfarsal@ya.ru>
2022-05-22 16:33:40 +0300

    CD-35 Eslint configuration. added .vscode to gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['src/**/*.g.ts'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
-      
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
